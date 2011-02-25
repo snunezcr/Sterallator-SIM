92ba45452f5a0d5d6937f5b61ec3ba2c9dd25df9
Santiago Nunez <snunezcr@gmail.com>
2011-02-25 13:33:10 -0600

    Agregando script para test 1

---
+++ src/testing/test_line.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Con poloidal = 0
+./test_point 0.238 0.040 12 725 0 90 0.000 ../coil_data > data.txt
+./test_point 0.238 0.040 12 725 0 90 0.002 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.004 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.006 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.008 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.010 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.012 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.014 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.016 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.018 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.020 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.022 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.024 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.026 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.028 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.030 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.032 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.034 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.036 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.038 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 90 0.040 ../coil_data >> data.txt
+
+# Con poloidal = 180
+./test_point 0.238 0.040 12 725 0 180 0.000 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.002 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.004 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.006 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.008 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.010 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.012 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.014 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.016 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.018 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.020 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.022 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.024 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.026 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.028 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.030 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.032 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.034 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.036 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.038 ../coil_data >> data.txt
+./test_point 0.238 0.040 12 725 0 180 0.040 ../coil_data >> data.txt
+
+
+
